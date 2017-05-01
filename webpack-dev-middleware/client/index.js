e69e83299fcec173c4255dca673d352269e4f44c
lazyTai <shuaigeliumingtai@163.com>
2017-05-01 16:58:28 +0800

    webpack-dev-middleware

---
+++ webpack-dev-middleware/client/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liumingtai on 2017-5-1.
+ callme 17602014220
+ github:https://github.com/lazyTai
+ */
+
+
+var index = require('./index1')
+
+debugger
---
+++ webpack-dev-middleware/client/index1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by liumingtai on 2017-5-1.
+ callme 17602014220
+ github:https://github.com/lazyTai
+ */
+module.exports={
+  a:"hello fuck111"
+}
